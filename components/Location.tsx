d495031a273e701139f6c723996f9901c2ceabd0
ShaikSameerHussain2104 <shaiksameerhussain2104@gmail.com>
2025-02-06 11:19:23 +0530

    Location updated

--- components/Location.tsx
+++ components/Location.tsx
@@ -13,7 +13,8 @@ export default function Location() {
       <h2 className="text-3xl font-bold mb-4 text-center">Location</h2>
       <motion.div whileHover={{ scale: 1.05 }} className="aspect-w-16 aspect-h-9">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3808.0448736030307!2d78.4885!3d17.3615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb98f7f39b8b67%3A0x3a4b6a7c4b5b9b1f!2sChandrayangutta%2C%20Hyderabad%2C%20Telangana!5e0!3m2!1sen!2sin!4v1625000000000!5m2!1sen!2sin"
+          // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3808.0448736030307!2d78.4885!3d17.3615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb98f7f39b8b67%3A0x3a4b6a7c4b5b9b1f!2sChandrayangutta%2C%20Hyderabad%2C%20Telangana!5e0!3m2!1sen!2sin!4v1625000000000!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3808.8664527379483!2d78.46779747493312!3d17.32199148355407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbbd5de573d34d%3A0xa85b1db8282c2821!2sMASJID-E-ALI%20SHEKHA!5e0!3m2!1sen!2sin!4v1738820794106!5m2!1sen!2sin"
           width="100%"
           height="100%"
           style={{ border: 0 }}
